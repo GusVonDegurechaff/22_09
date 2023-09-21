09e2dca9be8eeee9aa7908662882a4cd1c08d7d8
GusVonDegurechaff <115042946+GusVonDegurechaff@users.noreply.github.com>
2023-09-21 22:42:57 +0300

    Add files via upload

---
+++ c#dz11.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите минуты :");
+double m = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите a :");
+double a = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите b :");
+double b = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите c :");
+double c = Convert.ToDouble(Console.ReadLine());
+double res;
+if (m <= a)
+{
+    res = b;
+}
+else
+{
+    res = (m - a) * c + b;
+}
+Console.WriteLine($"В это месяце вы заплатите: {res}");
---
+++ c#dz6.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите m :");
+double m = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите n :");
+double n = Convert.ToDouble(Console.ReadLine());
+if (m != n)
+{
+    if (m > n)
+    {
+        Console.WriteLine("m больше");
+        n = m;
+    }
+    else
+    {
+        Console.WriteLine("n больше");
+        m = n;
+    }
+}
+else
+{
+    Console.WriteLine("m и n равны");
+    m = 0;
+    n = 0;
+}
+Console.WriteLine($"m = {m}");
+Console.WriteLine($"n = {n}");
